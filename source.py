d534ff9a25f524b6815fa69fe84bdcab39e17406
amoazeni75 <amoazeni75@gmail.com>
2019-09-20 11:16:09 +0430

    Update source.py

--- source.py
+++ source.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 10 16:23:18 2019
-
 @author: S.Alireza Moazeni(S.A.M.P.8)
+@tutorial source: Deep Learning With Python, Develop Deep Learning Models On Theano And TensorFlow Using
+Keras, Jason Brownlee
 """
 
 import numpy
